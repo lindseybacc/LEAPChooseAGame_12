ab958c20c99eed55b1e31778ae3926b7441cb6c6
Farouk Katakpaou <t-fkatakpaou@microsoft.com>
2025-02-05 20:58:30 -0500

    word class created

---
+++ Word_Search/Word.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_Search
+{
+    public class Word
+    {
+        private string text;
+        private string startPoint;
+        private string endPoint;
+
+        public Word(string word, string start, string end)
+        {
+            text = word; 
+            startPoint = start;
+            endPoint = end;
+        }
+
+        public int getLength()
+        {
+            return text.Length;
+        }
+
+        public bool isFound(string startPoint, string endPoint)
+        {
+            // Check in the list if word location matches 
+            return true;
+            // will implement the rest of the logic later
+        }
+
+
+    }
+}
